ca61168c1c23e33410299c236d4f42f884bf04ba
Paweł Rubach <pawel.rubach@gmail.com>
2020-05-19 11:28:04 +0200

    Working on deployment using swarm.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A development big data infrastructure with docker-compose.
 <br> Just run `docker-compose up` and enjoy!
 
 <br> You can find more explenation in [this article](https://itnext.io/creating-a-big-data-development-platform-using-docker-compose-892f7f4da738).
+
+
+http://localhost:50070 - NameNode
+http://localhost:8888/ - Hue
---
+++ telco_test.py
@@ -0,0 +1,19 @@
+%pyspark
+
+# File location and type
+file_location = "hdfs://namenode/user/admin/telco.csv"
+file_type = "csv"
+
+# CSV options
+infer_schema = "true"
+first_row_is_header = "true"
+delimiter = ","
+
+# The applied options are for CSV files. For other file types, these will be ignored.
+df = spark.read.format(file_type) \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load(file_location)
+
+df.show()
